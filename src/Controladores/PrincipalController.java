90f33d6af415cfc7de293a292744a2ec0870b622
Will <dev.willct@gmail.com>
2019-08-26 22:08:33 -0500

    Cambio de interfaz principal
    # implementación de logo e hipervínculos.

--- src/Controladores/PrincipalController.java
+++ src/Controladores/PrincipalController.java
@@ -3,6 +3,9 @@ package Controladores;
 import Utils.Pantalla;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXToggleButton;
+import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
+import com.sun.javafx.application.HostServicesDelegate;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
@@ -10,11 +13,18 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PrincipalController implements Initializable {
+public class PrincipalController extends Application implements Initializable {
 
     public JFXToggleButton tglDirigido;
     public JFXButton btnEmpezar;
     public JFXButton btnSalir;
+    public JFXButton btnContacto;
+    public JFXButton btnDocumentacion;
+    public JFXButton btnGithub;
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -22,10 +32,19 @@ public class PrincipalController implements Initializable {
         tglDirigido.setOnAction(event -> cambioEstadoDirigido());
         btnEmpezar.setOnAction(event -> abrirEspacioDeTrabajo());
         btnSalir.setOnAction(event -> terminarPrograma());
+
+        btnContacto.setOnAction(event -> abrirVinculo("https://mail.google.com/mail/?view=cm&fs=1&to=dev.willct@gmail.com&su=GraphFX%20consulta"));
+        btnDocumentacion.setOnAction(event -> abrirVinculo("https://drive.google.com/drive/folders/13R4WOlUrm1bke75ObX1HUXtUZhdXkJWw?usp=sharing"));
+        btnGithub.setOnAction(event -> abrirVinculo("https://github.com/Dev-will19/GraphFX"));
+    }
+
+    private void abrirVinculo(String url) {
+        HostServicesDelegate hostServices = HostServicesFactory.getInstance(this);
+        hostServices.showDocument(url);
     }
 
     private void cambioEstadoDirigido() {
-        if(tglDirigido.isSelected()) tglDirigido.setText("Grafo dirigido");
+        if (tglDirigido.isSelected()) tglDirigido.setText("Grafo dirigido");
         else tglDirigido.setText("Grafo no dirigido");
     }
 
@@ -39,5 +58,4 @@ public class PrincipalController implements Initializable {
         MesaTrabajoController mesaTrabajoController = loader.getController();
         mesaTrabajoController.establecerConfiguracion(tglDirigido.isSelected());
     }
-
 }
